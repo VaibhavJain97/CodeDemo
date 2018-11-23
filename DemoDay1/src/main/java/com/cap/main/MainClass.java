08e1758912cda5c75ff33c44698c54d494fcd405
vjain7 <vjain7@DIN73003014.corp.capgemini.com>
2018-11-23 14:32:18 +0530

    First Commit

---
+++ DemoDay1/src/main/java/com/cap/main/MainClass.java
@@ -0,0 +1,17 @@
+package com.cap.main;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hii! The Nucleya is here");
+		MainClass.greeting();
+		
+	}
+
+	private static void greeting() {
+		System.out.println("Heyy!! Mockito");
+		
+	}
+
+}
